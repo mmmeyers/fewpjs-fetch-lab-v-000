e161172a9868a6f23d34fd206a8c682c11d7169b
Marsi Meyers-Miyashita <marsi286@gmail.com>
2020-08-31 01:30:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
     .then(json => console.log(json));
-}
+https://learn.co/tracks/full-stack-web-development-v8/module-14-front-end-web-programming-in-javascript/section-4-communication-with-the-server/fetch-lab#}
 
 function renderBooks(json) {
   const main = document.querySelector('main')
